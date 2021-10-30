69edb80c48007db3194d70df9a7715df9154d222
locojoetive <johannes.mic@gmail.com>
2021-10-30 18:03:57 +0200

    rename TouchAndDestroy script and add property for shot-killing-behaviour

---
+++ Assets/_Scripts/GameMechanic/Enemies/TouchAndKill.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TouchAndKill : MonoBehaviour
+{
+    [SerializeField] private bool killByShot;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player") {
+            if (killByShot) collision.gameObject.GetComponent<NinjaStatesAnimationSound>().ShootNinja();
+            else collision.gameObject.GetComponent<NinjaStatesAnimationSound>().KillNinja();
+            Debug.Log("KILL");
+        }
+    }
+}
